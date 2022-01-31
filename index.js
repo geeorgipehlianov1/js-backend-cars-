c94322c4670c370918dccf427a7ec0856f4e0785
George <george9040@abv.bg>
2022-01-31 17:25:16 +0200

    Implemented car service home, detalis

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { home } = require('./controllers/home');
+const { notFound } = require('./controllers/notFound');
+const carsService = require('./services/cars')
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
+app.set('view engine', 'hbs');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+app.use(carsService())
+
+app.get('/', home);
+app.get('/about', about)
+app.get('/details/:id', details)
+app.get('/create', create)
+
+app.all('*', notFound)
+
+app.listen(3000, () => console.log('Server is listening on port 3000...'));
